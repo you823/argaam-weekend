adca26076d5ddaed5b928e54540fc58c9f9ff6b5
youssef elkholy <Youssef.elkholy@argaam.com>
2025-08-17 10:48:40 +0300

    add middleware.js file

--- middleware.js
+++ middleware.js
@@ -1,33 +1,42 @@
+import Negotiator from "negotiator";
+import { match } from "@formatjs/intl-localematcher";
 import { NextResponse } from "next/server";
-
-const PUBLIC_FILE = /\.(.*)$/; // Regex to exclude static files
-
+const locales = ["en", "ar"];
+const defaultLocale = "en";
+const cookieName = "i18nlang";
+function getLocale(request) {
+  if (request.cookies.has(cookieName)) {
+    return request.cookies.get(cookieName).value;
+  }
+  const acceptLang = request.headers.get("Accept-Language");
+  if (!acceptLang) return defaultLocale;
+  const headers = { "accept-language": acceptLang };
+  const languages = new Negotiator({ headers }).languages();
+  return match(languages, locales, defaultLocale);
+}
 export function middleware(request) {
   const { pathname } = request.nextUrl;
-
-  // Skip middleware for:
-  // 1. Static files (e.g., CSS, images, fonts)
-  // 2. API routes
-  // 3. Next.js internal paths (_next)
   if (
-    PUBLIC_FILE.test(pathname) ||
-    pathname.startsWith("/api/") ||
-    pathname.startsWith("/_next/")
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    pathname.includes(".")
   ) {
     return NextResponse.next();
   }
-
-  // Check if the URL has a supported language (en or ar)
-  const isLangSupported = ["en", "ar"].some(
-    (lang) => pathname.startsWith(`/${lang}/`) || pathname === `/${lang}`
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
 
-  // If the URL doesn't have a supported language, redirect to `/en`
-  if (!isLangSupported) {
-    const newUrl = request.nextUrl.clone();
-    newUrl.pathname = `/en${pathname}`;
-    return NextResponse.redirect(newUrl);
+  if (pathnameHasLocale) {
+    return NextResponse.next();
   }
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
 
-  return NextResponse.next();
+  const response = NextResponse.redirect(request.nextUrl);
+  response.cookies.set(cookieName, locale);
+  return response;
 }
+export const config = {
+  matcher: ["/((?!_next|api|favicon.ico|robots.txt|.*\\..*).*)"],
+};
